80f2d02831075ced1512a8f1a485c8bbc72e3da7
Максим <max.tkh.ua@gmail.com>
2015-12-04 08:45:08 +0200

    start

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,36 +1,16 @@
 $(function(){
 
-} );
+    $('.menu__btn').on({
+        'click':function(){
+            var curElem = $(this).parent();
 
-var Shablon = function (obj) {
-    this.obj = obj;
-
-
-    this.init();
-};
-Shablon.prototype = {
-    init: function () {
-        var self = this;
-
-        self.core = self.core();
-        self.core.build();
-    },
-    core: function () {
-        var self = this;
-
-        return {
-            addEvents: function () {
-
-            },
-            build: function () {
-                self.core.addEvents();
+            if (curElem.hasClass('active')) {
+                curElem.removeClass('active');
+            } else {
+                curElem.addClass('active');
             }
-        };
-    }
-};
 
-$(window).on({
-    load: function () {
-        
-    }
-});
+        }
+    });
+
+} );
