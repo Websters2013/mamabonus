587aec63d2279fc3aaba5b4b26d7fc5c321c6b97
einzweindrey <einzweindrey@gmail.com>
2015-12-08 18:25:46 +0200

    changed game.html, casinos.html,

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -24,22 +24,27 @@ $(function(){
 
     $(window).on({
         'load':function() {
-            var element_parent = $('.mama-metr__value'),
-                element_child = element_parent.children('div'),
-                element_parent_width = element_parent.width(),
-                element_child_width = element_child.width();
 
-            var element_width = (element_child_width / element_parent_width) * 100;
+            $.each( $('.mama-metr__value'), function(){
 
-            if (element_width <= 20) {
-                element_child.addClass('grey');
-            }
-            else if (element_width > 20 && element_width <= 60) {
-                element_child.addClass('blue');
-            }
-            else if (element_width > 40 && element_width <= 100) {
-                element_child.addClass('red');
-            }
+                var element_parent = $(this),
+                    element_child = element_parent.children('div'),
+                    element_parent_width = element_parent.width(),
+                    element_child_width = element_child.width();
+
+                var element_width = (element_child_width / element_parent_width) * 100;
+
+                if (element_width <= 20) {
+                    element_child.addClass('grey');
+                }
+                else if (element_width > 20 && element_width <= 60) {
+                    element_child.addClass('blue');
+                }
+                else if (element_width > 40 && element_width <= 100) {
+                    element_child.addClass('red');
+                }
+
+            } );
         }
     });
 
