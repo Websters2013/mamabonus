4f8cfdb45d1f8ce6969e8252b91e769b57ee9178
Максим <max.tkh.ua@gmail.com>
2015-12-08 23:39:45 +0200

    start

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -27,24 +27,10 @@ $(function(){
             element_parent_width = element_parent.width(),
             element_child_width = element_child.width();
 
-    $(window).on({
-        'load':function() {
-            var element_parent = $('.mama-metr__value'),
-                element_child = element_parent.children('div'),
-                element_parent_width = element_parent.width(),
-                element_child_width = element_child.width();
+        var element_width = (element_child_width / element_parent_width) * 100;
 
-            var element_width = (element_child_width / element_parent_width) * 100;
-
-            if (element_width <= 20) {
-                element_child.addClass('grey');
-            }
-            else if (element_width > 20 && element_width <= 60) {
-                element_child.addClass('blue');
-            }
-            else if (element_width > 40 && element_width <= 100) {
-                element_child.addClass('red');
-            }
+        if (element_width <= 20) {
+            element_child.addClass('grey');
         }
         else if (element_width > 20 && element_width <= 60) {
             element_child.addClass('blue');
