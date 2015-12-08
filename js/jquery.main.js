02b3cfb9943fdcdfbe8f55ea00e39ce9bb4e47e2
Максим <max.tkh.ua@gmail.com>
2015-12-08 16:49:17 +0200

    start

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,4 +1,9 @@
 $(function(){
+
+    $('.sub-menu').each(function () {
+        subMenu($(this));
+    });
+
     $('.menu__btn').on({
         'click':function(){
             var curElem = $(this).parent();
@@ -12,24 +17,27 @@ $(function(){
         }
     });
 
+    $('.mama-metr__value').each(function(){
+        var element_parent = $(this),
+            element_child = element_parent.children('div'),
+            element_parent_width = element_parent.width(),
+            element_child_width = element_child.width();
+
+        var element_width = (element_child_width/element_parent_width)*100;
+
+        if (element_width <= 20){
+            element_child.addClass('grey');
+        }
+        else if (element_width > 20 && element_width <= 60){
+            element_child.addClass('blue');
+        }
+        else if (element_width > 40 && element_width <= 100){
+            element_child.addClass('red');
+        }
+    })
+
     $(window).on({
         'load':function(){
-            var element_parent=$('.mama-metr__value'),
-                element_child=element_parent.children('div'),
-                element_parent_width=element_parent.width(),
-                element_child_width=element_child.width();
-
-            var element_width=(element_child_width/element_parent_width)*100;
-
-            if (element_width<=20){
-                element_child.addClass('grey');
-            }
-            else if (element_width>20&& element_width<=60){
-                element_child.addClass('blue');
-            }
-            else if (element_width>40&& element_width<=100){
-                element_child.addClass('red');
-            }
             if($(window).width() >= 900){
                 $('.menu-to-aside').clone().appendTo(".site__aside");
                 $('.menu-to-aside').addClass('aside__layout');
@@ -49,7 +57,6 @@ $(function(){
 
     function contentHeight() {
         if ($('.aside__layout').length) {
-            console.log($('.aside__layout').outerHeight())
             if ($('.site__content').outerHeight() <  $('.aside__layout').outerHeight()) {
                 var elemHeight = $('.aside__layout').outerHeight() + 160;
                 $('.site__content').css({
@@ -58,5 +65,57 @@ $(function(){
             }
         }
     }
+
+    var subMenu = function (obj) {
+        //private properties
+        var _obj = obj,
+            _site = $('.site'),
+            _btn = _obj.children('a'),
+            _sub = _obj.children('ul'),
+            _window = $(window),
+            _windowWidth = $(window).width();
+
+        //private methods
+        var _addEvents = function () {
+
+                _windowWidth = $(window).width();
+
+                _window.on({
+                    resize: function () {
+                        if(_windowWidth<=749){
+                            //$('.header__menu li').removeClass('active');
+                            //$('.header__menu li ul').css('display','none');
+                            _sub.css('display','block');
+                        }
+                    }
+                });
+
+                _btn.on({
+                    click: function () {
+                        if(_windowWidth<=749) {
+                            if (_obj.hasClass('mobile-active')) {
+                                _sub.slideUp(500);
+                                _obj.removeClass('mobile-active');
+                            } else {
+                                $('.menu li').removeClass('mobile-active');
+                                $('.menu li ul').slideUp(500);
+                                $(this).parent('li').addClass('mobile-active');
+                                _sub.slideDown(500);
+                            }
+                            return false
+                        }
+                    }
+                });
+            },
+            _init = function () {
+                _addEvents();
+            };
+
+        //public properties
+
+        //public methods
+
+        _init();
+    };
 
 } );
